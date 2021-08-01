1c4bca4eca0e6ebe02f3d158c0604d0faf1af75b
Tony Minseok Kim <tonykim.minseok@gmail.com>
2021-08-01 11:27:14 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+myAdj = input("What is your favourite adjective? \n")
+myVerb1 = input("What is your favourite action in a verb tense? \n")
+myVerb2 = input("What is your second favourite action in a verb tense? \n")
+myHero = input("Who is your favourite hero? \n")
+
+madlib = f"\nWe all know, We all feel the same, We all suffer... it's {myAdj}! \n \
+          I'm going to {myVerb1} with the moon tonight. \n \
+          Actually, the world will {myVerb2} it. \n \
+          Oh.. {myHero}, Help us if you're there. Be the hero for everyone's!!"
+
+print(madlib)
